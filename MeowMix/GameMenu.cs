79b2e51f34beba1e03f955269f34e6ac46b1e997
Montenegro <Ablauflogik@users.noreply.github.com>
2016-10-19 20:31:03 -0500

    Add files via upload

---
+++ MeowMix/GameMenu.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowMix
+{
+    class GameMenu
+    {
+       public static void Menu()
+        {
+            Console.Clear();
+            string userInput; 
+            Console.Title = "MeowMix";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            string title = @"                                                                            
+            @@@@@@@@@@   @@@@@@@@   @@@@@@   @@@  @@@  @@@  @@@@@@@@@@   @@@  @@@  @@@  
+            @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@  @@@@@@@@@@@  @@@  @@@  @@@  
+            @@! @@! @@!  @@!       @@!  @@@  @@!  @@!  @@!  @@! @@! @@!  @@!  @@!  !@@  
+            !@! !@! !@!  !@!       !@!  @!@  !@!  !@!  !@!  !@! !@! !@!  !@!  !@!  @!!  
+            @!! !!@ @!@  @!!!:!    @!@  !@!  @!!  !!@  @!@  @!! !!@ @!@  !!@   !@@!@!   
+            !@!   ! !@!  !!!!!:    !@!  !!!  !@!  !!!  !@!  !@!   ! !@!  !!!    @!!!    
+            !!:     !!:  !!:       !!:  !!!  !!:  !!:  !!:  !!:     !!:  !!:   !: :!!   
+            :!:     :!:  :!:       :!:  !:!  :!:  :!:  :!:  :!:     :!:  :!:  :!:  !:!  
+            :::     ::    :: ::::  ::::: ::   :::: :: :::   :::     ::    ::   ::  :::  
+             :      :    : :: ::    : :  :     :: :  : :     :      :    :     :   ::   ";
+            Console.WriteLine(title);           
+           // Console.ReadLine();
+
+
+            Console.WriteLine("\n Cats and dogs are at constant war. They don't want to kill but rather piss eachother off...You must decide which faction is best suited for you and continue the cycle of peskering one another!\n");
+
+            Console.WriteLine("Choose a faction to begin your mission!\n");
+            Console.Write("Choose between the FelineWarrior, Human Hunter or k9 faction\n");
+            userInput = Console.ReadLine().ToLower();
+
+
+            if (userInput == "felinewarrior")
+            {
+                string message = "NIIIIIICE! This factions uses hulking Whisker Brute and dander infested Fur Bawlz as its field troops. They have mind tricks naturally produced by their cuteness";
+                Console.WriteLine(message);
+                Console.ReadLine();
+                GamePartOne();
+                
+            }
+            else if (userInput == "human hunter")
+            {
+                string message = "Interesting choice, you have chosen to be a one man army. This pet hater of a human being roams the earth bagging and tagging stray pets. You will be frowned upon.";
+                Console.WriteLine(message);
+                Console.ReadLine();
+                GamePartOne();
+            }
+            else if (userInput == "k9")
+            {
+                string message = "Awesome sawce! You chose the cunning but also wreckless faction. Uses all natural senses as well as fleas to annoy their foes!";
+              
+                Console.WriteLine(message);
+                Console.ReadLine();
+                GamePartTwo();
+            }
+            else 
+            {
+                string message = "What? That's not an option. Maybe in the next game...";
+
+                Console.WriteLine(message);
+                Menu();
+            }
+            Console.WriteLine("\n Welcome to the game " + userInput + "!");
+            Console.ReadLine();
+        }
+
+        public static void GamePartOne()
+        {
+            Console.Clear();
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+
+            //Another choice like above
+        }
+
+
+        public static void GamePartTwo()
+        {
+            Console.Clear();
+            Console.WriteLine("sdffgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fsdfsfgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+
+            //Another choice like above
+        }
+        public static void IWENTLEFT()
+        {
+            Console.Clear();
+            Console.WriteLine("sdffgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fsdfsfgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+
+            //Another choice like above
+            Console.ReadLine();
+            MOREGENERALSTORY();
+        }
+        public static void IWENTRIGHT()
+        {
+            Console.Clear();
+            Console.WriteLine("sdffgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fsdfsfgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+
+            //Another choice like above
+            Console.ReadLine();
+            MOREGENERALSTORY();
+        }
+
+        public static void MOREGENERALSTORY()
+        {
+            Console.Clear();
+            Console.WriteLine("sdffgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fsdfsfgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+            Console.WriteLine("fgfjkasdgbfjksdbkfjbsdjkfbsdbfjksdbjkfbsdjkbfksd");
+
+            //Another choice like above
+        }
+    }
+}
---
+++ MeowMix/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowMix
+{
+    class Program
+    {
+       public static void Main()
+        {
+            GameMenu.Menu();
+            
+
+          
+            Console.ReadLine();
+
+            //FelineWarrior WhiskerBrute = new FelineWarrior("WhiskerBrute", 5, "wields itchy as hell whiskers!");
+            //WhiskerBrute.Run();
+
+            //FelineWarrior FurBawlz = new FelineWarrior("FurBawlz", 12, "releases a volley of fur bawls!");
+            //FurBawlz.Run();
+
+            HumanHunter HunterGuy = new HumanHunter();
+
+        }
+    }
+
+    class k9
+    {
+        string CharName;
+        int SprintSpeed;
+        string SpecialMove;
+
+        void Run()
+        {
+
+        }
+        void Bark()
+        {
+
+        }
+        void Bite()
+        {
+
+        }
+
+        
+
+    }
+
+    class FelineWarrior
+    {
+        string CharName;
+        int SprintSpeed;
+        string SpecialMove;
+
+        public FelineWarrior(string _charName, int _sprintSpeed, string _specialMove)
+        {
+            CharName = _charName;
+            SprintSpeed = _sprintSpeed;
+            SpecialMove = _specialMove;
+        }
+
+        public void Run()
+        {
+            Console.Write("\n" + CharName + " " + SprintSpeed + " " + SpecialMove + "\n");
+            Console.ReadLine();
+        }
+        void Hiss()
+        {
+
+        }
+        void Scratch()
+        {
+
+        }
+
+
+
+    }
+    class HumanHunter
+    {
+        string HunterName;
+        int SmackRadius;
+        string Job;
+
+        void Catch()
+        {
+            Console.WriteLine("");
+        }
+        void Release()
+        {
+            Console.WriteLine("");
+        }
+        void Search()
+        {
+            Console.WriteLine("");
+        }
+
+    }
+}
